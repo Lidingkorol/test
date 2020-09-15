edf3a48866adcf5986645827311336bc98702315
李丁 <liding@xinpibao.com>
2020-09-15 23:52:34 +0800

    ggg

--- App.vue
+++ App.vue
@@ -17,9 +17,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  font-size: 12px;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
