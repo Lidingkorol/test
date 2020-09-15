dac5d382aaf307d20d4a3c3575543c22a18ae7b2
李丁 <liding@xinpibao.com>
2020-09-15 23:57:08 +0800

    aha

--- App.vue
+++ App.vue
@@ -2,6 +2,7 @@
     <div id="app">
         <img src="./assets/logo.png">
         <router-view/>
+        <div></div>
     </div>
 </template>
 
@@ -16,6 +17,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  font-size: 12px;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
